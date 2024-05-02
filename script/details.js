4c89e620ea794eb03f0311c602d0fcb5a42a8da1
SNGCHN LEE <sngchn0312@naver.com>
2024-05-02 11:59:28 +0900

    feat: details

--- script/details.js
+++ script/details.js
@@ -2,21 +2,44 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTk5ZmRhOWE3MjY4ZTAwMTE3MTVhMjcxYjMzMTAwNSIsInN1YiI6IjY2MTdhMzllN2Q0MWFhMDE3ZDAwNzkxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.svBiDsUi6VuVUKtPq8DVrEd7G6rkepmITGwi_94lCk8",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTk5ZmRhOWE3MjY4ZTAwMTE3MTVhMjcxYjMzMTAwNSIsInN1YiI6IjY2MTdhMzllN2Q0MWFhMDE3ZDAwNzkxNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.svBiDsUi6VuVUKtPq8DVrEd7G6rkepmITGwi_94lCk8",
   },
 };
 
+async function fetchMovieDetails(movieID) {
+  try {
+    const url = `https://api.themoviedb.org/3/movie/${movieID}?language=ko-KR`;
+    const response = await fetch(url, options);
+    const movie = await response.json();
+    displayMovieDetails(movie);
+  } catch (error) {
+    return alert("영화 정보를 불러오는 데 실패했습니다.", error);
+  }
+}
+
+function displayMovieDetails(movie) {
+  document.getElementById("movie-title").textContent = movie.title;
+  document.getElementById("overview").textContent = movie.overview;
+  document.getElementById("genre").textContent = movie.genres.map((genre) => genre.name).join(", "); // 장르 배열을 문자열로 변환
+  document.getElementById("release-date").textContent = movie.release_date;
+  document.getElementById("runtime").textContent = movie.runtime + "분";
+  document.getElementById("rating").textContent = movie.vote_average;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const testMovieId = "550"; // 예: "파이트 클럽" 영화의 ID
+  fetchMovieDetails(testMovieId);
+  // const movieId = new URLSearchParams(window.location.search).get("id");
+  // fetchMovieDetails(movieId); // 영화 상세 정보 가져오기
+});
+
 async function creditApi() {
   try {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/movie/693134/credits?language=ko-KR",
-      options
-    );
+    const response = await fetch("https://api.themoviedb.org/3/movie/693134/credits?language=ko-KR", options);
     const data = await response.json();
     return data.cast;
   } catch (error) {
-    console.log(error);
+    console.log("영화 정보를 불러오는 데 실패했습니다.", error);
     return [];
   }
 }
