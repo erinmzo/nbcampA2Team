6fb1425a8a443afd207faa09d5657401714ec032
hyorimhan <mrahn102@gmail.com>
2024-05-02 12:33:49 +0900

    오류로 커밋

--- script/index.js
+++ script/index.js
@@ -23,15 +23,33 @@ async function movie() {
 async function movieRender() {
   const data = await movie();
   const cardList = data['results']
-  .map(({ original_title, poster_path, id }) => {
-    return `
+    .map(({ original_title, poster_path, id }) => {
+      return `
     <div class="movie-card">
       <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="poster" onclick="alert('${id}')"/>
       <h2 class="movie-title">${original_title}</h2>
     </div>`;
-  })
-  .join('');
+    })
+    .join('');
 
-document.getElementById('movie-card-list').innerHTML = cardList;
+  document.getElementById('movie-card-list').innerHTML = cardList;
 }
 movieRender();
+
+window.onscroll = function () {
+  let scroll = document.documentElement.scrollTop || document.body.scrollTop;
+  let gotop = document.getElementsByClassName('.btn_gotop');
+  if (scrollPosition > 700) {
+    btnGoTop.style.display = 'block';
+  } else {
+    btnGoTop.style.display = 'none';
+  }
+  document.querySelector('.btn_gotop').addEventListener('click', function (event) {
+    // 기본 이벤트를 방지합니다 (예: 링크 이동)
+    event.preventDefault();
+
+    // 부드럽게 상단으로 스크롤합니다.
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+}
+
