e4eb09b139c27c73d9fd3a0d4b389f2e61e3328a
Minsoo Cho <textign@gmail.com>
2024-05-03 10:18:46 +0900

    index.js 수정

--- script/index.js
+++ script/index.js
@@ -1,52 +1,48 @@
-document.addEventListener('DOMContentLoaded', function () {
-  movie();
-  movieRender();
-});
-
 window.onscroll = function () {
   document
-    .getElementsByClassName('btn_gotop')
-    .addEventListener('click', function (e) {
+    .getElementsByClassName("up-btn")
+    .addEventListener("click", function (e) {
       e.preventDefault();
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: "smooth" });
     });
 };
 
-
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
+    accept: "application/json",
     Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTNmNTQ0YzM0NzgxODcxMzgxMmNhOTg4MTk2ZDY4ZCIsInN1YiI6IjY2MzBhMGEwNTU0NWNhMDEyZDQzOWJmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PCSH53do58eERB95XaVTj4PWERd4-VPCIJGu_iMongM',
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYTNmNTQ0YzM0NzgxODcxMzgxMmNhOTg4MTk2ZDY4ZCIsInN1YiI6IjY2MzBhMGEwNTU0NWNhMDEyZDQzOWJmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PCSH53do58eERB95XaVTj4PWERd4-VPCIJGu_iMongM",
   },
 };
 
 export async function movie() {
   const url =
-    'https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1';
+    "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1";
 
   try {
     const response = await fetch(url, options);
     const data = await response.json();
-    return data;
+    return data.results;
   } catch (error) {
     return alert(error);
   }
 }
-
-async function movieRender() {
+async function movieData() {
   const data = await movie();
-  const cardList = data['results']
+  movieRender(data);
+}
 
-    .map(({ title, poster_path, id }) => {
-      return `<div class="movie-card">
+function movieRender(movie) {
+  const cardList = movie.map(({ title, poster_path, id }) => {
+    return `<div class="movie-card">
       <img src="https://image.tmdb.org/t/p/original${poster_path}" alt="poster" onclick="alert('${id}')"/>
       <h2 class="movie-title">${title}</h2>
     </div>`;
-    })
-    .join('');
+  });
+  cardList.join("");
 
-  document.getElementById('movie-card-list').innerHTML = cardList;
+  document.getElementById("movie-card-list").innerHTML = cardList;
 }
 
+movieData();
